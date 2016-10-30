b24b07595be4581ca7d787022954c9a303c40aba
MGVera <madelinegvera@gmail.com>
2016-10-30 00:26:51 -0400

    got the admin and app working

--- tutorial_project/tutorial_app/views.py
+++ tutorial_project/tutorial_app/views.py
@@ -5,70 +5,71 @@ from forms import CategoryForm, PageForm
 
 
 def index(request):
-	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {}
-	context_dict['categories'] = category_list
-	page_list = Page.objects.order_by('-views')[:5]
-	context_dict['pages'] = page_list
+		category_list = Category.objects.order_by('-likes')[:5]
+		context_dict = {}
+		context_dict['categories'] = category_list
+		page_list = Page.objects.order_by('-views')[:5]
+		context_dict['pages'] = page_list
 
 
-	return render(request, 'index.html', context_dict)
+		return render(request, 'index.html', context_dict)
 
 def about(request):
-	return HttpResponse('About Us')
+		return HttpResponse('About Us')
 
 def category(request, category_name_slug):
-	context_dict = {}
-	try:
+		context_dict = {}
+		try:
 		category = Category.objects.get(slug=category_name_slug)
 		pages = Page.objects.filter(category=category)
 
 		context_dict['category'] = category
 		context_dict['pages'] = pages
 
-	except Category.DoesNotExist:
-		pass
+		except Category.DoesNotExist:
+				pass
 
-	return render(request, 'category.html', context_dict)
+		return render(request, 'category.html', context_dict)
 
 def add_category(request):
-	if request.method == 'POST':
-		form = CategoryForm(request.POST)
-		if form.is_valid():
-			form.save(commit=True)
-			return index(request)
+		if request.method == 'POST':
+				form = CategoryForm(request.POST)
+				if form.is_valid():
+						form.save(commit=True)
+						return index(request)
+				else:
+						print form.errors
 		else:
-			print form.errors
-	else:
-		form = CategoryForm()
+				form = CategoryForm()
 
-	return render(request, 'add_category.html', {'form':form})
+		return render(request, 'add_category.html', {'form':form})
 
 def add_page(request, category_name_slug):
-	try:
-		cat = Category.objects.get(slug=category_name_slug)
-	except Category.DoesNotExist:
-		cat = None
+		try:
 
-	if request.method == 'POST':
-		form =  PageForm(request.POST)
+				cat = Category.objects.get(slug=category_name_slug)
+		except Category.DoesNotExist:
+				cat = None
 
-		if form.is_valid():
-			if cat:
-				page = form.save(commit=False)
-				page.category = cat
-				page.views = 0
-				page.save()
-				return category(request, category_name_slug)
-			else:
-				print form.errors
+		if request.method == 'POST':
+				form =  PageForm(request.POST)
+
+				if form.is_valid():
+						if cat:
+								page = form.save(commit=False)
+								page.category = cat
+								page.views = 0
+								page.save()
+								return category(request, category_name_slug)
+						else:
+								print form.errors
+				else:
+						print forms.errors
 		else:
-			print forms.errors
-	else:
-		form = PageForm()
+				form = PageForm()
 
-	context_dict = {'form':form, 'category':cat, 'slug':category_name_slug}
-	return render(request, 'add_page.html', context_dict)
+		context_dict = {'form':form, 'category':cat, 'slug':category_name_slug}
+		return render(request, 'add_page.html', context_dict)
 
 	
 
